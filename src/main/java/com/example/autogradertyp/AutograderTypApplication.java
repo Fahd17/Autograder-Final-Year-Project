48c4d7bddaf23f1d623f7aab9b8a687135a1440a
Fahd17 <fahdsattam8@gmail.com>
2022-11-13 23:16:30 +0000

    Change theme to dark theme

--- src/main/java/com/example/autogradertyp/AutograderTypApplication.java
+++ src/main/java/com/example/autogradertyp/AutograderTypApplication.java
@@ -1,10 +1,17 @@
 package com.example.autogradertyp;
 
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Theme(variant = Lumo.DARK)
+@JsModule("@vaadin/vaadin-lumo-styles/presets/compact.js")
 @SpringBootApplication
-public class AutograderTypApplication {
+public class AutograderTypApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(AutograderTypApplication.class, args);
