7b70a32d620d18633d79634c575dcc5e48118b69
Fahd17 <fahdsattam8@gmail.com>
2022-11-20 00:43:33 +0000

    Css automatically generated

---
+++ submissions_directory/p111.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p111 {
+
+    public static void main(String[] args) {
+	Scanner myObj = new Scanner(System.in);  
+    
+
+    int num = myObj.nextInt();  
+		System.out.println(24);
+
+    }
+}
